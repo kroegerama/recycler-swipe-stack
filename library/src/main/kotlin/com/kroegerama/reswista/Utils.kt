d080e0de8bfcbb5a592eb095f52bb9017bdc6c4c
kroegerama <1519044+kroegerama@users.noreply.github.com>
2021-03-03 09:29:48 +0100

    add helper function for one-liner setup

---
+++ library/src/main/kotlin/com/kroegerama/reswista/Utils.kt
@@ -0,0 +1,14 @@
+package com.kroegerama.reswista
+
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+fun RecyclerView.setupStack(
+    config: SwiperConfig,
+    listener: SwipeListener,
+    block: (RecyclerView.() -> Unit)? = null
+) {
+    layoutManager = StackLayoutManager(config)
+    ItemTouchHelper(StackSwipeTouchHelperCallback(listener, config)).attachToRecyclerView(this)
+    block?.invoke(this)
+}
