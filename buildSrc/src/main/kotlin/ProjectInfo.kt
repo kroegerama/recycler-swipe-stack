52a36a15a9e1918d3f0d8461c034d155908d2b77
kroegerama <1519044+kroegerama@users.noreply.github.com>
2021-03-03 09:32:12 +0100

    increase version

--- buildSrc/src/main/kotlin/ProjectInfo.kt
+++ buildSrc/src/main/kotlin/ProjectInfo.kt
@@ -1,5 +1,5 @@
 object P {
-    const val projectVersion = "0.9.0"
+    const val projectVersion = "0.9.1"
 
     const val projectGroupId = "com.kroegerama"
     const val libraryArtifactId = "recycler-swipe-stack"
